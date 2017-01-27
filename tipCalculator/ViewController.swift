531b0e9a63646838c73e5a0b0ffb3e5a2a11d86f
Joshua Hudson <jhudy11@hotmail.com>
2017-01-26 20:13:21 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Joshua Hudson on 1/26/17.
+//  Copyright © 2017 com.ParanoidPenguinProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
